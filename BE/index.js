c9a1ba6d9d6d6191b2c5f2db6e678fcfe9292e96
iamankr09 <iamankr09@gmail.com>
2023-07-08 20:51:02 +0530

    Amancommit

---
+++ BE/index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const {Server} = require("socket.io");
+const http = require("http");
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+    
+
+    socket.on("joinRoom", room => socket.join(room))
+
+    socket.on("newMessage", ({newMessage, room}) => {
+        io.in(room).emit("getLatestMessage", newMessage)
+    })
+  
+  });
+
+  app.get("/", (req, res) => {res.send("Chat BE with Socket.io by Aman Kumar"); res.end()});
+
+
+
+ const port = process.env.PORT || 8001
+
+ server.listen(port, console.log(`App started at port ${port}`))
