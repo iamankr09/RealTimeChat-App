c940b742a15588e876f1b458cc7c6167814400fd
Aman kumar <108830462+iamankr09@users.noreply.github.com>
2023-07-09 00:54:58 +0530

    Update ChatRoom.jsx

--- FE/socket-chat-fe/src/component/ChatRoom.jsx
+++ FE/socket-chat-fe/src/component/ChatRoom.jsx
@@ -16,7 +16,7 @@ const ChatRoom = () => {
 
     useEffect(() => {
         // const socket = io("http://localhost:8001/");
-         const socket = io('http://localhost:9000',{transports: ['websocket']});
+         const socket = io('http://localhost:9000/',{transports: ['websocket']});
         setSocket(socket)
         socket.on("connect", () => {
             console.log(socket.id)
